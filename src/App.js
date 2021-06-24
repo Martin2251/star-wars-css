0e3f01551d7209a892c1dedf0cae98b2412dcba2
martin lowe <martinlowe673@gmail.com>
2021-06-24 07:05:12 +0100

    added card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
+function Card(props) {
+  return (
+    <div className="card">
+      <h4>{props.character.name}</h4>
+      <p>{props.character.height}</p>
+      <p>{props.character.birth_year}</p>
+      <p>{props.character.films.length}</p>
+    </div>
+  );
+}
 function App() {
+  const [listCharacters, setListCharacters] = useState([]);
+  const [nextUrl, setNextUrl] = useState("");
+
+  useEffect(function () {
+    fetch("https://swapi.dev/api/people/")
+      .then(function (response) {
+        return response.json();
+      })
+      .then((data) => {
+        setListCharacters(data.results);
+        setNextUrl(data.next);
+      });
+  }, []);
+  // will change the state of the app
+  function loadMore() {
+    // fetch next URL
+    fetch(nextUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      //update the state
+      .then(function (data) {
+        setListCharacters([...listCharacters, ...data.results]);
+        setNextUrl(data.next);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card-container">
+        {" "}
+        {listCharacters.map(function (character) {
+          return <Card character={character}></Card>;
+        })}
+      </div>
+
+      <button onClick={loadMore}>Load More</button>
     </div>
   );
 }
